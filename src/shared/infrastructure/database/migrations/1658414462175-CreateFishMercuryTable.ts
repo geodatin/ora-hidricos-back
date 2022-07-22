a7e738fd39fc614cb9ed2d5511e05409b8ab6322
Neto1904 <netohsa66@gmail.com>
2022-07-22 13:35:29 -0300

    feat: add migration to create new tables

---
+++ src/shared/infrastructure/database/migrations/1658414462175-CreateFishMercuryTable.ts
@@ -0,0 +1,92 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateFishMercuryTable1658414462175 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'fish_mercury',
+        columns: [
+          {
+            name: 'code',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'location',
+            type: 'geometry',
+            isNullable: true,
+          },
+          {
+            name: 'publication_year',
+            type: 'integer',
+            isNullable: true,
+          },
+          {
+            name: 'study',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'author',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'sample_number',
+            type: 'integer',
+            isNullable: true,
+          },
+          {
+            name: 'hg_min',
+            type: 'double precision',
+            isNullable: true,
+          },
+          {
+            name: 'hg_max',
+            type: 'double precision',
+            isNullable: true,
+          },
+          {
+            name: 'hg_mean',
+            type: 'double precision',
+            isNullable: true,
+          },
+          {
+            name: 'hg_median',
+            type: 'double precision',
+            isNullable: true,
+          },
+          {
+            name: 'collection_year',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'community',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'measurement_unit',
+            type: 'varchar',
+            isNullable: true,
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('fish_mercury')
+  }
+}
