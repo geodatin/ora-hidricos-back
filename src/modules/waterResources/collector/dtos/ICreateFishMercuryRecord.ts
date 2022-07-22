128014606869accb8e9c27d10153976b68eabb44
Neto1904 <netohsa66@gmail.com>
2022-07-22 13:31:25 -0300

    feat: add DTO to create record on table

---
+++ src/modules/waterResources/collector/dtos/ICreateFishMercuryRecord.ts
@@ -0,0 +1,16 @@
+export interface ICreateFishMercuryRecord {
+  study: string
+  author: string
+  title: string
+  state: string
+  sampleNumber: number
+  hgMin: number
+  hgMax: number
+  hgMean: number
+  hgMedian: number
+  collectionYear: string
+  community: string
+  publicationYear: number
+  latitude: number
+  longitude: number
+}
