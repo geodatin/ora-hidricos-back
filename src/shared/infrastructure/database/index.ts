cf229b42c1d140d308bb6a57d2472d6f81edb8f0
Neto1904 <netohsa66@gmail.com>
2022-07-22 13:35:10 -0300

    perf: change database logger

--- src/shared/infrastructure/database/index.ts
+++ src/shared/infrastructure/database/index.ts
@@ -1,7 +1,9 @@
 import env from 'dotenv-safe'
 import { createConnection } from 'typeorm'
 
+import { databaseLog } from '@shared/utils/log'
+
 env.config()
-createConnection().then(() => {
-  console.log('Connection established')
+createConnection().then((connection) => {
+  databaseLog(connection)
 })
