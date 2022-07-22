cbc5edc5ca1188f175b7e305577745a0bc2ad36e
Neto1904 <netohsa66@gmail.com>
2022-07-22 13:30:45 -0300

    feat: add fish mercury repository

---
+++ src/modules/waterResources/api/infrastructure/models/FishMercury.ts
@@ -0,0 +1,57 @@
+import { Point } from 'geojson'
+import { Entity, PrimaryColumn, Column } from 'typeorm'
+import { v4 as uuidv4 } from 'uuid'
+
+@Entity({ name: 'fish_mercury', schema: process.env.SCHEMA })
+export class FishMercury {
+  @PrimaryColumn()
+  code: string
+
+  @Column({ type: 'geometry', srid: 4326, nullable: true })
+  location: Point
+
+  @Column({ name: 'publication_year' })
+  publicationYear: number
+
+  @Column()
+  study: string
+
+  @Column()
+  author: string
+
+  @Column()
+  title: string
+
+  @Column()
+  state: string
+
+  @Column({ name: 'sample_number' })
+  sampleNumber: number
+
+  @Column({ name: 'hg_min' })
+  hgMin: number
+
+  @Column({ name: 'hg_max' })
+  hgMax: number
+
+  @Column({ name: 'hg_mean' })
+  hgMean: number
+
+  @Column({ name: 'hg_median' })
+  hgMedian: number
+
+  @Column({ name: 'collection_year' })
+  collectionYear: string
+
+  @Column()
+  community: string
+
+  @Column({ name: 'measurement_unit' })
+  measurementUnit: string
+
+  constructor() {
+    if (!this.code) {
+      this.code = uuidv4()
+    }
+  }
+}
