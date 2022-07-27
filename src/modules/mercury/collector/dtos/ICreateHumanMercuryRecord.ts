a7053f116eec8e32089a17ae4c3f940b28ed331a
Neto1904 <netohsa66@gmail.com>
2022-07-27 15:36:22 -0300

    feat: add dtos to create mercury contamination record

---
+++ src/modules/mercury/collector/dtos/ICreateHumanMercuryRecord.ts
@@ -0,0 +1,17 @@
+export interface ICreateHumanMercuryRecord {
+  study: string
+  author: string
+  title: string
+  state: string
+  ageGroup: string
+  sampleNumber: number
+  hgMin: number
+  hgMax: number
+  hgMean: number
+  hgMedian: number
+  collectionYear: string
+  community: string
+  publicationYear: number
+  latitude: number
+  longitude: number
+}
