7047987bc4a38f16eafb98fb0c36eb3269558383
Neto1904 <netohsa66@gmail.com>
2022-07-27 17:14:39 -0300

    fix: remodeling project folders

--- src/modules/waterResources/collector/repositories/IFishMercuryRepository.ts
+++
@@ -1,5 +0,0 @@
-import { ICreateFishMercuryRecord } from '../dtos/ICreateFishMercuryRecord'
-
-export interface IFishMercuryRepository {
-  create(data: ICreateFishMercuryRecord[]): Promise<void>
-}
--- src/modules/waterResources/collector/repositories/IHumanMercuryRepository.ts
+++
@@ -1,5 +0,0 @@
-import { ICreateHumanMercuryRecord } from '../dtos/ICreateHumanMercuryRecord'
-
-export interface IHumanMercuryRepository {
-  create(data: ICreateHumanMercuryRecord[]): Promise<void>
-}
