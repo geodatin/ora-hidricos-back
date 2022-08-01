768e4e7d4c0660155c410eba163097db24d68591
Neto1904 <netohsa66@gmail.com>
2022-08-01 09:20:22 -0300

    feat: implement api repository

---
+++ src/modules/oil/api/infrastructure/repositories/OilFieldRepositoryApi.ts
@@ -0,0 +1,40 @@
+import { getRepository, Repository } from 'typeorm'
+
+import { IGetOilFieldPointsDTO } from '../../dtos/IGetOilFieldPointsDTO'
+import { IOilFieldRepositoryApi } from '../../repositories/IOilFieldRepositoryApi'
+import { OilField } from '../models/OilField'
+
+export class OilFieldRepositoryApi implements IOilFieldRepositoryApi {
+  private repository: Repository<OilField>
+
+  constructor() {
+    this.repository = getRepository(OilField)
+  }
+
+  async getPoints({ code }: IGetOilFieldPointsDTO): Promise<OilField[]> {
+    const oilFieldsQuery = this.repository
+      .createQueryBuilder('oil_field')
+      .select('code', 'code')
+      .addSelect('country', 'country')
+      .addSelect('name', 'name')
+      .addSelect('company', 'company')
+      .addSelect('situation', 'situation')
+      .addSelect('source', 'source')
+      .addSelect('institution', 'institution')
+
+    if (code) {
+      oilFieldsQuery
+        .addSelect('ST_AsGeoJSON(geometry)::json', 'geometry')
+        .where('code = :code', { code })
+    } else {
+      oilFieldsQuery.addSelect(
+        'ST_AsGeoJSON(ST_Centroid(geometry))::json',
+        'geometry'
+      )
+    }
+
+    const oilFields = await oilFieldsQuery.getRawMany()
+
+    return oilFields
+  }
+}
