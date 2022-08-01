ef530361ef71580647c7d2eb704c56b4b4006b19
Neto1904 <netohsa66@gmail.com>
2022-08-01 17:25:21 -0300

    feat: add api repository

---
+++ src/modules/mining/api/infrastructure/repositories/IllegalMiningRepositoryApi.ts
@@ -0,0 +1,44 @@
+import { getRepository, Repository } from 'typeorm'
+
+import { IGetIllegalMiningPointsDTO } from '../../dtos/IGetIllegalMiningPointsDTO'
+import { IIllegalMiningRepositoryApi } from '../../repositories/IIllegalMiningRepositoryApi'
+import { IllegalMining } from '../models/IllegalMining'
+
+export class IllegalMiningRepositoryApi implements IIllegalMiningRepositoryApi {
+  private repository: Repository<IllegalMining>
+
+  constructor() {
+    this.repository = getRepository(IllegalMining)
+  }
+  async getPoints({
+    code,
+  }: IGetIllegalMiningPointsDTO): Promise<IllegalMining[]> {
+    const illegalMiningQuery = this.repository
+      .createQueryBuilder('illegal_mining')
+      .select('code', 'code')
+      .addSelect('name', 'name')
+      .addSelect('description', 'description')
+      .addSelect('exploration_method', 'explorationMethod')
+      .addSelect('substance', 'substance')
+      .addSelect('contamination', 'contamination')
+      .addSelect('situation_end', 'situationEnd')
+      .addSelect('information_source', 'informationSource')
+      .addSelect('institution', 'institution')
+      .addSelect('link', 'link')
+
+    if (code) {
+      illegalMiningQuery
+        .addSelect('ST_AsGeoJSON(geometry)::json', 'geometry')
+        .where('code = :code', { code })
+    } else {
+      illegalMiningQuery.addSelect(
+        'ST_AsGeoJSON(ST_Centroid(geometry))::json',
+        'geometry'
+      )
+    }
+
+    const illegalMining = await illegalMiningQuery.getRawMany()
+
+    return illegalMining
+  }
+}
