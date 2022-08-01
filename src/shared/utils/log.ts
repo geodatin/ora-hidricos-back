1b4c28d837e907fe3c2e7c3016417a92937dfaef
Neto1904 <netohsa66@gmail.com>
2022-08-01 09:27:33 -0300

    feat: add new logger

--- src/shared/utils/log.ts
+++ src/shared/utils/log.ts
@@ -2,5 +2,6 @@ import debbuger from 'debug'
 
 const collectorLog = debbuger('collector')
 const databaseLog = debbuger('database')
+const appLog = debbuger('app')
 
-export { collectorLog, databaseLog }
+export { collectorLog, databaseLog, appLog }
