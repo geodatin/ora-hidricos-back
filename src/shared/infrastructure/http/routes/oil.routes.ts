73aa5776813619e04b18b6a3df06fb88aeae85d8
Neto1904 <netohsa66@gmail.com>
2022-08-23 14:49:29 -0300

    feat: add routes to new methods

--- src/shared/infrastructure/http/routes/oil.routes.ts
+++ src/shared/infrastructure/http/routes/oil.routes.ts
@@ -1,8 +1,14 @@
+import { GetCompanyRankingController } from '@modules/oil/api/services/getCompanyRanking/GetCompanyRankingController'
 import { GetOilFieldPointsController } from '@modules/oil/api/services/getOilFieldPoints/GetOilFieldPointsController'
+import { GetTotalOilFieldController } from '@modules/oil/api/services/getTotalOilFiled/GetTotalOilFiledController'
 import { Router } from 'express'
 
 export const oilRoutes = Router()
 
 const getOilFieldPointsController = new GetOilFieldPointsController()
+const getTotalOilFieldController = new GetTotalOilFieldController()
+const getCompanyRankingController = new GetCompanyRankingController()
 
 oilRoutes.get('/field/points', getOilFieldPointsController.handle)
+oilRoutes.get('/field/total', getTotalOilFieldController.handle)
+oilRoutes.get('/field/ranking', getCompanyRankingController.handle)
