4b89810fdd2763ced8de395dd6dda2b45893e246
Neto1904 <netohsa66@gmail.com>
2022-08-24 11:01:54 -0300

    feat: add route for situation method

--- src/shared/infrastructure/http/routes/oil.routes.ts
+++ src/shared/infrastructure/http/routes/oil.routes.ts
@@ -1,5 +1,6 @@
 import { GetCompanyRankingController } from '@modules/oil/api/services/getCompanyRanking/GetCompanyRankingController'
 import { GetOilFieldPointsController } from '@modules/oil/api/services/getOilFieldPoints/GetOilFieldPointsController'
+import { GetSituationAmountController } from '@modules/oil/api/services/getSituationAmount/GetSituationAmountController'
 import { GetTotalOilFieldController } from '@modules/oil/api/services/getTotalOilFiled/GetTotalOilFiledController'
 import { Router } from 'express'
 
@@ -8,7 +9,9 @@ export const oilRoutes = Router()
 const getOilFieldPointsController = new GetOilFieldPointsController()
 const getTotalOilFieldController = new GetTotalOilFieldController()
 const getCompanyRankingController = new GetCompanyRankingController()
+const getSituationAmountController = new GetSituationAmountController()
 
 oilRoutes.get('/field/points', getOilFieldPointsController.handle)
 oilRoutes.get('/field/total', getTotalOilFieldController.handle)
 oilRoutes.get('/field/ranking', getCompanyRankingController.handle)
+oilRoutes.get('/field/situation', getSituationAmountController.handle)
