9ce1d3cdc8f42b5918af0960d72d53da0c1af25a
Neto1904 <netohsa66@gmail.com>
2022-08-29 18:05:01 -0300

    chore: add util to insert shapefile

---
+++ src/shared/infrastructure/database/utils/insertShapefile.ts
@@ -0,0 +1,20 @@
+import 'dotenv-safe/config'
+import { exec } from 'child_process'
+import util from 'util'
+
+const execPromise = util.promisify(exec)
+
+export async function insertShapefile(
+  shapefilePath: string,
+  tableName: string,
+  srid: number,
+  schema = 'hydric',
+  encoding = 'UTF8'
+) {
+  const { stderr: error } = await execPromise(
+    `shp2pgsql -I -s ${srid}:4326 -W "${encoding}" ${shapefilePath} ${schema}.${tableName} | PGPASSWORD=${process.env.DB_PASSWORD} psql -q -d ${process.env.DB_NAME} -U ${process.env.DB_USERNAME} -h ${process.env.DB_HOST}`
+  )
+  if (error) {
+    console.log(error)
+  }
+}
