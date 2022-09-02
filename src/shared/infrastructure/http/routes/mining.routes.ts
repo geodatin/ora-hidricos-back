5a522cda458ac03a6cb76d777e4b7088b73c0304
Neto1904 <netohsa66@gmail.com>
2022-09-02 15:30:06 -0300

    Add mining mine routes

--- src/shared/infrastructure/http/routes/mining.routes.ts
+++ src/shared/infrastructure/http/routes/mining.routes.ts
@@ -1,8 +1,12 @@
 import { GetIllegalMiningPointsController } from '@modules/mining/api/services/getIllegalMiningPoints/GetIllegalMiningPointsController'
 import { GetIllegalMiningTimeSeriesController } from '@modules/mining/api/services/getIllegalMiningTimeSeries/GetIllegalMiningTimeSeriesController'
+import { GetMinesByCountryController } from '@modules/mining/api/services/getMinesByCountry/GetMinesByCountryController'
 import { GetMiningMinePointsController } from '@modules/mining/api/services/getMiningMinePoints/GetMiningMinePointsController'
+import { GetSituationMapController } from '@modules/mining/api/services/getSituationMap/GetSituationMapController'
 import { GetSubstanceRankingController } from '@modules/mining/api/services/getSubstanceRanking/GetSubstanceRankingController'
 import { GetTotalIllegalMiningOccurrencesController } from '@modules/mining/api/services/getTotalllegalMiningOccurrences/GetTotalIllegalMiningOccurrencesController'
+import { GetTotalMiningMineOccurrencesController } from '@modules/mining/api/services/getTotalMiningMineOccurrences/GetTotalMiningMineOccurrencesController'
+import { GetCompanyRankingController } from '@modules/oil/api/services/getCompanyRanking/GetCompanyRankingController'
 import { Router } from 'express'
 
 import { checkTile } from '../middlewares/checkTile'
@@ -17,6 +21,11 @@ const getIllegalMiningTimeSeriesController =
 const getSubstanceRankingController = new GetSubstanceRankingController()
 
 const getMiningMinePointsController = new GetMiningMinePointsController()
+const getTotalMiningMineOccurrencesController =
+  new GetTotalMiningMineOccurrencesController()
+const getCompanyRankingController = new GetCompanyRankingController()
+const getMinesByCountryController = new GetMinesByCountryController()
+const getSituationMapController = new GetSituationMapController()
 
 miningRoutes.get('/illegal/points', getIllegalMiningPointsController.handle)
 miningRoutes.get(
@@ -36,3 +45,11 @@ miningRoutes.get(
   checkTile,
   getMiningMinePointsController.handle
 )
+
+miningRoutes.get(
+  '/mine/total/occurrences',
+  getTotalMiningMineOccurrencesController.handle
+)
+miningRoutes.get('/mine/company/ranking', getCompanyRankingController.handle)
+miningRoutes.get('/mine/countries', getMinesByCountryController.handle)
+miningRoutes.get('/mine/situation', getSituationMapController.handle)
