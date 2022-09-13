c6d8b5a9f25a4682153c6dc8c410178e10326878
Neto1904 <netohsa66@gmail.com>
2022-09-13 17:22:33 -0300

    Add waterway tiles route

--- src/shared/infrastructure/http/routes/waterway.routes.ts
+++ src/shared/infrastructure/http/routes/waterway.routes.ts
@@ -1,4 +1,5 @@
 import { GetShapeAsMvtController } from '@modules/waterResources/api/services/getShapeAsMvt/GetShapeAsMvtController'
+import { GetShapeAsTilesController } from '@modules/waterResources/api/services/getShapeAsTiles/GetShapeAsGeoJsonController'
 import { Router } from 'express'
 
 import { checkTile } from '../middlewares/checkTile'
@@ -6,9 +7,12 @@ import { checkTile } from '../middlewares/checkTile'
 export const waterwayRoutes = Router()
 
 const getShapeAsMvtController = new GetShapeAsMvtController()
+const getShapeAsTilesController = new GetShapeAsTilesController()
 
 waterwayRoutes.get(
   '/tiles/:z/:x/:y.:format',
   checkTile,
   getShapeAsMvtController.handle
 )
+
+waterwayRoutes.get('/tiles/image', getShapeAsTilesController.handle)
