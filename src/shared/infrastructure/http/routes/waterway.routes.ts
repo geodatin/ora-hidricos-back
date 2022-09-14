50a26d030ae33e4977d520175816784fb45c3bde
Neto1904 <netohsa66@gmail.com>
2022-09-14 11:18:21 -0300

    Add route to get tile property

--- src/shared/infrastructure/http/routes/waterway.routes.ts
+++ src/shared/infrastructure/http/routes/waterway.routes.ts
@@ -1,3 +1,4 @@
+import { GetPropertiesController } from '@modules/waterResources/api/services/getProperties/GetPropertiesController'
 import { GetShapeAsMvtController } from '@modules/waterResources/api/services/getShapeAsMvt/GetShapeAsMvtController'
 import { GetShapeAsTilesController } from '@modules/waterResources/api/services/getShapeAsTiles/GetShapeAsGeoJsonController'
 import { Router } from 'express'
@@ -8,6 +9,7 @@ export const waterwayRoutes = Router()
 
 const getShapeAsMvtController = new GetShapeAsMvtController()
 const getShapeAsTilesController = new GetShapeAsTilesController()
+const getPropertiesController = new GetPropertiesController()
 
 waterwayRoutes.get(
   '/tiles/:z/:x/:y.:format',
@@ -16,3 +18,7 @@ waterwayRoutes.get(
 )
 
 waterwayRoutes.get('/tiles/image', getShapeAsTilesController.handle)
+waterwayRoutes.get(
+  '/properties/tiles/:long/:lat',
+  getPropertiesController.handle
+)
