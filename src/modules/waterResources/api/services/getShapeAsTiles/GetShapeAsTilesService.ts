b8af32e6fbe73f36ca7f6af28cbb7881d6f76711
Neto1904 <netohsa66@gmail.com>
2022-09-19 14:38:01 -0300

    Remove useless constructor

--- src/modules/waterResources/api/services/getShapeAsTiles/GetShapeAsTilesService.ts
+++ src/modules/waterResources/api/services/getShapeAsTiles/GetShapeAsTilesService.ts
@@ -3,8 +3,6 @@ import { injectable } from 'tsyringe'
 
 @injectable()
 export class GetShapeAsTilesService {
-  constructor() {}
-
   async execute() {
     const featureCollection = ee.FeatureCollection(
       'projects/ora-rh/assets/indicators/waterways'
