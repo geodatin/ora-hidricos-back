3cb13080b657b9d60348cb110ba0aa3dc724861d
Neto1904 <netohsa66@gmail.com>
2022-09-20 12:08:43 -0300

    Add pollution routes

--- src/shared/infrastructure/http/routes/pollution.routes.ts
+++ src/shared/infrastructure/http/routes/pollution.routes.ts
@@ -1,20 +1,20 @@
+import { GetPropertiesController } from '@modules/pollution/services/getProperties/GetPropertiesController'
 import { GetShapeController } from '@modules/pollution/services/getShape/getShapeController'
 import { GetSituationMapController } from '@modules/pollution/services/getSituationMap/GetSituationMapController'
 import { GetTotalPointsController } from '@modules/pollution/services/getTotalPoints/GetTotalPointsController'
 import { Router } from 'express'
 
-import { checkTile } from '../middlewares/checkTile'
-
 export const pollutionRoutes = Router()
 
 const getShapeController = new GetShapeController()
+const getPropertiesController = new GetPropertiesController()
 const getTotalPointsController = new GetTotalPointsController()
 const getSituationMapController = new GetSituationMapController()
 
-pollutionRoutes.get(
-  '/tiles/:z/:x/:y.:format',
-  checkTile,
-  getShapeController.handle
-)
 pollutionRoutes.get('/total', getTotalPointsController.handle)
 pollutionRoutes.get('/situation', getSituationMapController.handle)
+pollutionRoutes.get('/tiles', getShapeController.handle)
+pollutionRoutes.get(
+  '/tiles/properties/:long/:lat',
+  getPropertiesController.handle
+)
