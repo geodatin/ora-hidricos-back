16e3446751078bd0b273dc9384ac7758d7498bb8
Neto1904 <netohsa66@gmail.com>
2022-09-21 15:13:36 -0300

    Add nre feature route

--- src/shared/infrastructure/http/routes/population.routes.ts
+++ src/shared/infrastructure/http/routes/population.routes.ts
@@ -1,3 +1,4 @@
+import { GetPropertiesController } from '@modules/population/services/getProperties/GetPropertiesController'
 import { GetShapeController } from '@modules/population/services/getShape/GetShapeController'
 import { GetTotalController } from '@modules/population/services/getTotal/GetTotalController'
 import { GetWatershedRankingController } from '@modules/population/services/getWatershedRanking /GetWatershedRankingController'
@@ -8,7 +9,12 @@ export const populationRoutes = Router()
 const getShapeController = new GetShapeController()
 const getTotalController = new GetTotalController()
 const getWatershedRankingController = new GetWatershedRankingController()
+const getPropertiesController = new GetPropertiesController()
 
 populationRoutes.get('/tiles', getShapeController.handle)
+populationRoutes.get(
+  '/tiles/properties/:long/:lat',
+  getPropertiesController.handle
+)
 populationRoutes.get('/total', getTotalController.handle)
 populationRoutes.get('/ranking', getWatershedRankingController.handle)
