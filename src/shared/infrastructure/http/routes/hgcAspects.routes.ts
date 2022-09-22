ed8ffa13565ad8c95949d475b2d4587ed4669100
Neto1904 <netohsa66@gmail.com>
2022-09-22 11:58:42 -0300

    Add new hgc routes

--- src/shared/infrastructure/http/routes/hgcAspects.routes.ts
+++ src/shared/infrastructure/http/routes/hgcAspects.routes.ts
@@ -1,3 +1,5 @@
+import { GetAspectsRankingController } from '@modules/hgcAspects/services/getAspectsRanking/GetAspectsRankingController'
+import { GetDomainRankingController } from '@modules/hgcAspects/services/getDomainRanking/GetDomainRankingController'
 import { GetShapeController } from '@modules/hgcAspects/services/getShape/GetShapeController'
 import { GetTotalController } from '@modules/hgcAspects/services/getTotal/GetTotalController'
 import { Router } from 'express'
@@ -6,6 +8,10 @@ export const hgcAspectsRoutes = Router()
 
 const getShapeController = new GetShapeController()
 const getTotalController = new GetTotalController()
+const getDomainRankingController = new GetDomainRankingController()
+const getAspectsRankingController = new GetAspectsRankingController()
 
 hgcAspectsRoutes.get('/shape', getShapeController.handle)
 hgcAspectsRoutes.get('/total', getTotalController.handle)
+hgcAspectsRoutes.get('/ranking/domain', getDomainRankingController.handle)
+hgcAspectsRoutes.get('/ranking/aspects', getAspectsRankingController.handle)
