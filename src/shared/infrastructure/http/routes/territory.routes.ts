2c2711e7e3f537b42166d89678276fa14cd2e83b
Neto1904 <netohsa66@gmail.com>
2022-09-23 14:26:05 -0300

    Add watershed routes

--- src/shared/infrastructure/http/routes/territory.routes.ts
+++ src/shared/infrastructure/http/routes/territory.routes.ts
@@ -1,8 +1,32 @@
+import { GetAreaByCountryController } from '@modules/territory/services/getAreaByCountryCountry/GetAreaByCountryController'
+import { GetCountryRankingController } from '@modules/territory/services/getCountryRanking/GetCountryRankingController'
+import { GetPropertiesController } from '@modules/territory/services/getProperties/GetPropertiesController'
+import { GetShapeController } from '@modules/territory/services/getShape/GetShapeController'
 import { GetTerritoryNameController } from '@modules/territory/services/getTerritoryNames/GetTerritoryNameController'
+import { GetTotalWatershedAreaController } from '@modules/territory/services/getTotalWatershedArea/GetTotalWatershedAreaController'
 import { Router } from 'express'
 
 export const territoryRoutes = Router()
 
 const getTerritoryNameController = new GetTerritoryNameController()
+const getTotalWatershedAreaController = new GetTotalWatershedAreaController()
+const getAreaByCountryController = new GetAreaByCountryController()
+const getCountryRankingController = new GetCountryRankingController()
+const getShapeController = new GetShapeController()
+const getPropertiesController = new GetPropertiesController()
 
 territoryRoutes.get('/:name', getTerritoryNameController.handle)
+territoryRoutes.get('/watershed/area', getTotalWatershedAreaController.handle)
+territoryRoutes.get(
+  '/watershed/area/country',
+  getAreaByCountryController.handle
+)
+territoryRoutes.get(
+  '/watershed/area/country/ranking',
+  getCountryRankingController.handle
+)
+territoryRoutes.get('/watershed/tiles', getShapeController.handle)
+territoryRoutes.get(
+  '/watershed/tiles/properties/:long/:lat',
+  getPropertiesController.handle
+)
