dfac822b13758eb1bca285052c21f4084e848cd4
Neto1904 <netohsa66@gmail.com>
2022-09-23 14:24:47 -0300

    Add new method on repository

--- src/modules/territory/infrastructure/typeorm/repositories/AmazonCountryRepository.ts
+++ src/modules/territory/infrastructure/typeorm/repositories/AmazonCountryRepository.ts
@@ -10,6 +10,37 @@ export class AmazonCountryRepository implements IAmazonCountryRepository {
     this.repository = getRepository(AmazonCountry)
   }
 
+  async getAreaByCountry(): Promise<any[]> {
+    const results = await this.repository
+      .createQueryBuilder('country')
+      .select('perc_pais', 'count')
+      .addSelect('code', 'countryCode')
+      .addSelect('name', 'country')
+      .getRawMany()
+
+    return results
+  }
+
+  async getCountryRanking(): Promise<any[]> {
+    const results = await this.repository
+      .createQueryBuilder('country')
+      .select('km2_amazo', 'amount')
+      .addSelect('name', 'name')
+      .orderBy('km2_amazo', 'DESC')
+      .getRawMany()
+
+    return results
+  }
+
+  async getTotalWatershedArea(): Promise<number> {
+    const { count } = await this.repository
+      .createQueryBuilder('country')
+      .select('SUM(km2_amazo)', 'count')
+      .getRawOne()
+
+    return count
+  }
+
   async getNames(
     name: string
   ): Promise<{ code: string; name: string; type: string }[]> {
--- src/modules/territory/repositories/IAmazonCountryRepository.ts
+++ src/modules/territory/repositories/IAmazonCountryRepository.ts
@@ -1,5 +1,13 @@
+export interface IRanking {
+  name: string
+  amount: number
+}
+
 export interface IAmazonCountryRepository {
   getNames(
     name: string
   ): Promise<{ code: string; name: string; type: string }[]>
+  getTotalWatershedArea(): Promise<number>
+  getAreaByCountry(): Promise<any[]>
+  getCountryRanking(): Promise<IRanking[]>
 }
