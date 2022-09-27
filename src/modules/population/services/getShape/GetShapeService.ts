b46f408301e027edc9edd0eee44d599c09bf0d2b
Neto1904 <netohsa66@gmail.com>
2022-09-27 09:48:17 -0300

    Return shape painted by property

--- src/modules/population/services/getShape/GetShapeService.ts
+++ src/modules/population/services/getShape/GetShapeService.ts
@@ -7,10 +7,21 @@ export class GetShapeService {
     const featureCollection = ee.FeatureCollection(
       'projects/ora-rh/assets/indicators/population'
     )
-    const { urlFormat: url } = featureCollection.getMap({
+    const image = featureCollection
+      .reduceToImage({
+        properties: ['total'],
+        reducer: ee.Reducer.first(),
+      })
+      .visualize({
+        min: 132,
+        max: 5633481,
+        palette: ['FCFDBF', 'FDAE78', 'EE605E', 'B63679', '711F81', '2C105C'],
+      })
+
+    const { urlFormat: url } = image.getMap({
       format: 'png',
-      color: 'blue',
     })
+
     return { url }
   }
 }
