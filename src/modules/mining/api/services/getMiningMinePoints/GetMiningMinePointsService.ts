cd67fdb3c8cd11a12d9ef4834d157bcd54b908c8
Neto1904 <netohsa66@gmail.com>
2022-12-22 15:06:21 -0300

    Paint features before generating map

--- src/modules/mining/api/services/getMiningMinePoints/GetMiningMinePointsService.ts
+++ src/modules/mining/api/services/getMiningMinePoints/GetMiningMinePointsService.ts
@@ -6,10 +6,30 @@ export class GetMiningMinePointsService {
   constructor() {}
 
   async execute() {
-    const featureCollection = ee.FeatureCollection(
-      'projects/ora-rh/assets/indicators/mining-mine'
-    )
-    const { urlFormat: url } = featureCollection.getMap({
+    const featureCollection = ee
+      .FeatureCollection('projects/ora-rh/assets/indicators/mining')
+      .filterMetadata('fecha_atua', 'contains', '2020')
+      .filterMetadata('situacion', 'not_contains', 'sin dato')
+
+    const palette = ee.Dictionary({
+      'en exploraciÃ³n': 'cca667',
+      'en explotaciÃ³n': '883367',
+      'en exploraciÃ³n/en explotaciÃ³n': 'c65390',
+      solicitud: '344a8a',
+      potencial: 'cdcf37',
+      'concesiÃ³n sin actividad': 'efaaae',
+    })
+
+    const image = featureCollection
+      .map(function (feature) {
+        return feature.set('myStyle', {
+          color: palette.get(feature.get('leyenda')),
+        })
+      })
+      .style({
+        styleProperty: 'myStyle',
+      })
+    const { urlFormat: url } = image.getMap({
       format: 'png',
       color: 'green',
     })
